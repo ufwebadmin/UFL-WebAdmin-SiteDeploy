f79898c4b11e73434e0d15cac3a5eee36a173038
Daniel Westermann-Clark <dwc@ufl.edu>
2009-05-11 19:13:49 +0000

    Convert execute override to around and reorganize for clarity

--- lib/SVN/Notify/Mirror/Rsync/AutoCheckout.pm
+++ lib/SVN/Notify/Mirror/Rsync/AutoCheckout.pm
@@ -5,26 +5,29 @@ use Cwd ();
 
 extends 'SVN::Notify::Mirror::Rsync';
 
+with 'UFL::WebAdmin::SiteDeploy::Role::CreateCheckout',
+    'UFL::WebAdmin::SiteDeploy::Role::SwitchCheckout',
+    'UFL::WebAdmin::SiteDeploy::Role::LogCommit';
+
+# Register arguments for SVN::Notify
 __PACKAGE__->register_attributes(
     repos_uri    => 'repos-uri:s',
     tag_pattern  => 'tag-pattern:s',
     log_category => 'log-category:s',
 );
 
-override 'execute' => sub {
-    my ($self) = @_;
+around 'execute' => sub {
+    my $next = shift;
+    my $self = $_[0];
 
     warn "Log category is not set" unless $self->log_category;
 
+    # SVN::Notify::Mirror switches to the configured path, so put us
+    # back where we started
     my $cwd = Cwd::getcwd();
-    super();
+    $next->(@_);
     chdir($cwd);
 };
-
-# XXX: Apply roles after overriding execute; otherwise Moose barfs
-with 'UFL::WebAdmin::SiteDeploy::Role::CreateCheckout',
-    'UFL::WebAdmin::SiteDeploy::Role::SwitchCheckout',
-    'UFL::WebAdmin::SiteDeploy::Role::LogCommit';
 
 =head1 NAME
 
@@ -48,23 +51,6 @@ anything else.
 Additionally, it improves upon the tag handling of
 L<SVN::Notify::Mirror>, which forces a specific repository structure
 on the user.
-
-=head1 METHODS
-
-=head2 execute
-
-Override L<SVN::Notify/execute> to set up default values for certain
-attributes registered via L<SVN::Notify/register_attributes>.
-
-=over 4
-
-=item * C<log_category> - the value of C<__PACKAGE__>
-
-=back
-
-Additionally, override L<SVN::Notify::Mirror/execute> to return to the
-previous working directory. Normally, L<SVN::Notify::Mirror/execute>
-switches to the configured path.
 
 =head1 AUTHOR
 
